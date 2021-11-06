329b17765c800521bf0e22e2f5613e1504a4df79
Janiru Vishmith <me@janiru.dev>
2021-11-06 10:13:55 +0000

    Pages and Routes

---
+++ src/pages/projects/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <h3>Projects & Website I've Created</h3>
+    </div>
+  )
+}
