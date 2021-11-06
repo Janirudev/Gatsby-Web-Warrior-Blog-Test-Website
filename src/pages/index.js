6a3e6a02db45a55625c84c6c9508d6989fea496c
Janiru Vishmith <me@janiru.dev>
2021-11-06 16:29:05 +0000

    Static Files

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
           <p>UX designer & web developer based in Belfast</p>
           <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
+        <img src="./banner.png" alt="Site Banner" style={{maxWidth: '100%'}}/>
       </section>
     </Layout>
   )
